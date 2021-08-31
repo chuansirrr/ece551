b5eea9a80d29e66b2aba9a47d3e7a375d32b627d
sx80 <sx80@duke.edu>
2021-08-31 17:39:34 -0400

    first commit

--- 013_complex_ty/mytypes.c
+++ 013_complex_ty/mytypes.c
@@ -1,7 +1,7 @@
-typedef struct _complex_number_t {
+struct _complex_number_t {
   double real_part;
   double imaginary_part;
-} complex_number_t;
-
+};
+typedef struct _complex_number_t complex_number_t;
 enum _animal_t { CAT, FISH, SNAK, DOG, HORSE };
 typedef enum _animal_t animal_t;
