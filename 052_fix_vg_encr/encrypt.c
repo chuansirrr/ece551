5f6e0c1fecadaa99baff765aa42bffd94d97bd0d
sx80 <sx80@duke.edu>
2021-09-29 16:41:32 -0400

    first

--- 052_fix_vg_encr/encrypt.c
+++ 052_fix_vg_encr/encrypt.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 void encrypt(FILE * f, int key, FILE * outfile) {
-  char * line;
+  char * line = NULL;
   size_t sz;
   while (getline(&line, &sz, f) >= 0) {
     char * ptr = line;
