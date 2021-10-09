962039c559dd20f667c88f3a02b19d0d20a9f27f
sx80 <sx80@duke.edu>
2021-10-08 22:35:13 -0400

    first commit

--- 056_kvs/kv.c
+++ 056_kvs/kv.c
@@ -1,22 +1,100 @@
+#include "kv.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "kv.h"
 
+kvpair_t parseKeyVals(char * line) {
+  kvpair_t kvPair;
+  kvPair.key = NULL;
+  kvPair.value = NULL;
 
+  size_t keyLen = 0;
+  while (*line != '=' && *line != '\0') {
+    kvPair.key = realloc(kvPair.key, (keyLen + 1) * sizeof(kvPair.key));
+    kvPair.key[keyLen] = *line;
+    keyLen++;
+    line++;
+  }
+
+  kvPair.key[keyLen] = '\0';
+
+  line++;
+
+  if (strchr(line, '\n') != NULL) {
+    *strchr(line, '\n') = '\0';
+  }
+
+  kvPair.value = strdup(line);
+
+  return kvPair;
+}
 
 kvarray_t * readKVs(const char * fname) {
   //WRITE ME
+  FILE * f = fopen(fname, "r");
+  if (f == NULL) {
+    fprintf(stderr, "Could not open file");
+    exit(EXIT_FAILURE);
+  }
+
+  kvarray_t * kvArray = malloc(sizeof(*kvArray));
+  kvArray->kvPairs = NULL;
+
+  char * curr = NULL;
+  size_t linecap;
+  size_t numPairs = 0;
+
+  while (getline(&curr, &linecap, f) >= 0) {
+    kvArray->kvPairs =
+        realloc(kvArray->kvPairs, (numPairs + 1) * sizeof(*kvArray->kvPairs));
+    kvArray->kvPairs[numPairs] = parseKeyVals(curr);
+
+    free(curr);
+
+    curr = NULL;
+    numPairs++;
+  }
+
+  free(curr);
+  kvArray->numPairs = numPairs;
+
+  // exit with error if file can't be closed
+  if (fclose(f) != 0) {
+    fprintf(stderr, "Failed to close the input file!");
+    exit(EXIT_FAILURE);
+  }
+
+  return kvArray;
 }
 
 void freeKVs(kvarray_t * pairs) {
   //WRITE ME
+  for (size_t i = 0; i < pairs->numPairs; i++) {
+    free(pairs->kvPairs[i].key);
+    free(pairs->kvPairs[i].value);
+  }
+
+  free(pairs->kvPairs);
+  free(pairs);
 }
 
 void printKVs(kvarray_t * pairs) {
   //WRITE ME
+  for (size_t i = 0; i < pairs->numPairs; i++) {
+    printf("key = '%s' value = '%s'\n", pairs->kvPairs[i].key, pairs->kvPairs[i].value);
+  }
 }
 
 char * lookupValue(kvarray_t * pairs, const char * key) {
   //WRITE ME
+  char * value = NULL;
+
+  for (size_t i = 0; i < pairs->numPairs; i++) {
+    if (!strcmp(key, pairs->kvPairs[i].key)) {
+      value = pairs->kvPairs[i].value;
+    }
+  }
+
+  return value;
 }
--- 056_kvs/kv.h
+++ 056_kvs/kv.h
@@ -1,20 +1,20 @@
+#include <stdlib.h>
 #ifndef __KV_H__
 #define __KV_H__
 
-
-struct _kvpair_t { 
+struct _kvpair_t {
   //DEFINE ME!
-
+  char * key;
+  char * value;
 };
 typedef struct _kvpair_t kvpair_t;
 
-struct _kvarray_t { 
+struct _kvarray_t {
   //DEFINE ME!
-
-
+  size_t numPairs;
+  kvpair_t * kvPairs;
 };
 typedef struct _kvarray_t kvarray_t;
-
 
 kvarray_t * readKVs(const char * fname);
 
